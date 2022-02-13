0cdefc15e83744db56f38c6cf38a426447c3a1b8
Theo Gregory <tjmg@outlook.com>
2022-02-13 20:26:12 +0000

    Most the way there, padding issue

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import styled from 'styled-components';
 
-import { COLORS } from '../../constants';
+import {COLORS} from '../../constants';
 import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import {getDisplayedValue} from './Select.helpers';
 
-const Select = ({ label, value, onChange, children }) => {
+const StyledSelect = styled.select`
+  background-color: ${COLORS.transparentGray15};
+  border: none;
+  border-radius: 8px;
+  padding: 12px 16px;
+  color: ${COLORS.gray700};
+
+  &:focus {
+    border: 2px solid #4374CB;
+  }
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`
+
+const Select = ({label, value, onChange, children}) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
+    <StyledSelect value={value} onChange={onChange}>
       {children}
-    </select>
+    </StyledSelect>
   );
 };
 
