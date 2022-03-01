a809334bd2c852615f5593aa0b88532950c2f1b7
Theo Gregory <tjmg@outlook.com>
2022-03-01 08:19:02 +0000

    All done except onClick

--- src/components/IconInput/IconInput.jsx
+++ src/components/IconInput/IconInput.jsx
@@ -3,21 +3,36 @@ import styled from "styled-components";
 
 import { COLORS } from "../../constants";
 
-import Icon from "../Icon";
+import UnstyledIcon from "../Icon";
 import VisuallyHidden from "../VisuallyHidden";
-
-const Input = styled.input`
-  display: inline;
-  border: none;
-`;
 
 const Wrapper = styled.div`
   border-bottom: var(--borderWidth) solid #000000;
   width: var(--width);
+  display: flex;
+  gap: 8px;
+  color: ${COLORS.gray700};
 
-  & > ${Input}:focus {
-    outline: 2px solid #4374cb;
+  &:hover {
+    color: black;
   }
+
+  &:focus-within {
+    outline: 2px solid #4374cb;
+    outline-offset: 2px;
+  }
+`;
+
+const Icon = styled(UnstyledIcon)`
+  margin-block: auto;
+  color: inherit;
+`;
+
+const Input = styled.input`
+  border: none;
+  outline: none;
+  margin-block: 4px;
+  color: inherit;
 `;
 
 const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
@@ -39,8 +54,14 @@ const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
         "--width": width + "px",
       }}
     >
-      <Icon id={icon} strokeWidth={style.strokeWidth} size={style.iconSize} />
+      <Icon
+        id={icon}
+        strokeWidth={style.strokeWidth}
+        size={style.iconSize}
+        onClick={() => {}}
+      />
       <Input placeholder={placeholder} />
+      <VisuallyHidden>Search input</VisuallyHidden>
     </Wrapper>
   );
 };
